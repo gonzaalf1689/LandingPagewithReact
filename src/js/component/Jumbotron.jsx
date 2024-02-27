893eb23db39283c72ff2a13fa242a66a300d4e4a
Alfredo <106854439+gonzaalf1689@users.noreply.github.com>
2024-02-27 03:11:59 +0000

    Updated navbar and Jumbotron

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 // a function component 
 const Jumbotron = () =>{
-    return (<div className="p-5 mb-4 bg-body-tertiary rounded-3">
+    return (<div className="p-5 mb-4 bg-body rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+        <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
         <p className="col-md-8 fs-4">"Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking."</p>
-        <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
       </div>
     </div>
     );
