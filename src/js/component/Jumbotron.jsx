1e8f92307116f30d0c91b103678d6775d2d198e1
Alfredo <106854439+gonzaalf1689@users.noreply.github.com>
2024-02-28 01:01:49 +0000

    changed colors and styling for jumbotron and navbar

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 // a function component 
 const Jumbotron = () =>{
-    return (<div className="p-5 mb-4 bg-body rounded-3">
+    return (<div className="p-5 ms-5 me-5 mb-5 rounded-3" style={{ backgroundColor: 'lightgrey' }}>
       <div className="container-fluid py-5">
         <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
-        <p className="col-md-8 fs-4">"Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking."</p>
-        <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+        <p className="fs-3 mt-2">"Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking."</p>
+        <button className="btn btn-primary btn-lg mt-2" type="button">Call to action!</button>
       </div>
     </div>
     );
--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -2,21 +2,24 @@ import React from "react";
 
 // a function component 
 const NavBar = () =>{
-    return (<nav className="navbar navbar-expand-lg bg-body-tertiary ms-5 me-5">
-	<div className="container-fluid">
-	  <a className="navbar-brand" href="#">Start Bootstrap</a>
-	  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		<span className="navbar-toggler-icon"></span>
-	  </button>
-	  <div className="collapse d-flex" id="navbarNavAltMarkup">
-		<div className="navbar-nav">
-		  <a className="nav-link active" aria-current="page" href="#">Home</a>
-		  <a className="nav-link" href="#">About</a>
-		  <a className="nav-link" href="#">Services</a>
-		  <a className="nav-link" aria-disabled="true">Contact</a>
-		</div>
-	  </div>
-	</div>
-  </nav>);
+    return (
+        <nav className="navbar navbar-expand-lg" style={{ position: 'fixed', top: 0, left: 0, width: '100%', backgroundColor: 'black'}}>
+            <div className="container-fluid ms-5 me-5">
+                <a className="navbar-brand" href="#" style={{ color: 'white' }}>Start Bootstrap</a>
+                <button className="navbar-toggler" style={{ color: 'white' }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse d-flex" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#" style={{ color: 'white' }}>Home</a>
+                        <a className="nav-link" href="#" style={{ color: 'gray' }}>About</a>
+                        <a className="nav-link" href="#" style={{ color: 'gray' }}>Services</a>
+                        <a className="nav-link" href="#" style={{ color: 'gray' }}>Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
+
 export default NavBar;
